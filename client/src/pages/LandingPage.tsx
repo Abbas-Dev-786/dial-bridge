342d2283ccda9e0fdad30595c4f2cc88a4085528
Abbas Bhanpura wala <99265597+Abbas-Dev-786@users.noreply.github.com>
2026-04-19 08:59:30 +0530

    mod: removed pricing section

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -18,7 +18,7 @@ export default function LandingPage() {
       <StatsSection />
       <FeatureSection />
       <HowItWorksSection />
-      <PricingSection />
+      {/* <PricingSection /> */}
       <TestimonialSection />
       <CTASection />
       <Footer />
